ac5b747fc7979c20e0faaaa1d2e60a6021372701
Wenderson <wenderson_fagundes@hotmail.com>
2024-05-20 08:22:51 -0300

    feat: implementar contrato

--- README.md
+++ README.md
@@ -25,4 +25,29 @@ source .venv/bin/activate
 ```bash
 pip install -r requeriments.txt
 ```
+6. Seguindo o padrão de commit pattern
+```bash
+Tipos de Commits
+O commit possui os elementos estruturais abaixo (tipos), que informam a intenção do seu commit ao utilizador(a) de seu código.
 
+fix - Commits do tipo fix indicam que seu trecho de código commitado está solucionando um problema (bug fix), (se relaciona com o PATCH do versionamento semântico).
+
+feat- Commits do tipo feat indicam que seu trecho de código está incluindo um novo recurso (se relaciona com o MINOR do versionamento semântico).
+
+docs - Commits do tipo docs indicam que houveram mudanças na documentação, como por exemplo no Readme do seu repositório. (Não inclui alterações em código).
+
+style - Commits do tipo style indicam que houveram alterações referentes a formatações de código, semicolons, trailing spaces, lint... (Não inclui alterações em código).
+
+refactor - Commits do tipo refactor referem-se a mudanças devido a refatorações que não alterem sua funcionalidade, como por exemplo, uma alteração no formato como é processada determinada parte da tela, mas que manteve a mesma funcionalidade, ou melhorias de performance devido a um code review.
+
+build - Commits do tipo build são utilizados quando são realizadas modificações em arquivos de build e dependências.
+
+test - Commits do tipo test são utilizados quando são realizadas alterações em testes, seja criando, alterando ou excluindo testes unitários. (Não inclui alterações em código)
+
+chore - Commits do tipo chore indicam atualizações de tarefas de build, configurações de administrador, pacotes... como por exemplo adicionar um pacote no gitignore. (Não inclui alterações em código)
+
+```
+7. Exemplo de commit
+```bash
+git commit -m "docs: subindo readme"
+```
---
+++ tests/test_contrato.py
@@ -0,0 +1,75 @@
+import pytest
+from datetime import datetime
+from src.contrato import Vendas
+from pydantic import ValidationError
+
+def test_vendas_com_dados_validos():
+
+    dados_validos = {
+        "email": "comprador@example.com",
+        "data": datetime.now(),
+        "valor": 100.50,
+        "produto": "Produto X",
+        "quantidade": 3,
+        "categoria": "categoria3",
+    }
+
+    venda = Vendas(**dados_validos)
+
+    assert venda.email == dados_validos["email"]
+    assert venda.data == dados_validos["data"]
+    assert venda.valor == dados_validos["valor"]
+    assert venda.produto == dados_validos["produto"]
+    assert venda.quantidade == dados_validos["quantidade"]
+    assert venda.categoria == dados_validos["categoria"]
+    
+
+def test_vendas_com_dados_invalidos():
+    """
+    Testa a criação de uma instância da classe Vendas com dados inválidos.
+
+    Este teste verifica a validação de dados na classe Vendas. Dados inválidos incluem um email incorreto, 
+    data em formato inválido, valor negativo, nome do produto vazio, quantidade negativa, e uma categoria válida.
+    Espera-se que a classe Vendas gere uma exceção ValidationError quando dados inválidos são fornecidos.
+    """
+    dados_invalidos = {
+        "email": "comprador",
+        "data": "não é uma data",
+        "valor": -100,
+        "produto": "",
+        "quantidade": -1,
+        "categoria": "categoria3"
+    }
+
+    with pytest.raises(ValidationError):
+        Vendas(**dados_invalidos)
+
+def test_validacao_categoria():
+    """
+    Testa a validação da categoria na criação de uma instância da classe Vendas.
+
+    Este teste especificamente verifica se a classe Vendas valida a categoria do produto. 
+    Ele utiliza dados válidos para todos os campos, exceto pela categoria, que é definida como uma categoria inexistente.
+    Espera-se que a classe Vendas gere uma exceção ValidationError devido à categoria inválida.
+
+    args:
+        dados = {
+        email: comprador@example.com
+        data: datetime.now()
+        valor: 100.50
+        produto: Produto Y
+        quantidade: 1
+        categoria: categoria inexistente
+    }
+    """
+    dados = {
+        "email": "comprador@example.com",
+        "data": datetime.now(),
+        "valor": 100.50,
+        "produto": "Produto Y",
+        "quantidade": 1,
+        "categoria": "categoria inexistente",
+    }
+
+    with pytest.raises(ValidationError):
+        Vendas(**dados)
