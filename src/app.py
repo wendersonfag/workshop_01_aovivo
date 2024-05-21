4f2d58bef5b39d881c4c7ab44da2a055e7909292
Wenderson <wenderson_fagundes@hotmail.com>
2024-05-21 18:28:03 -0300

    buil: adicionando sentry

--- src/app.py
+++ src/app.py
@@ -1,6 +1,18 @@
 from frontend import ExcelValidorUI
 from backend import process_excel, excel_to_sql
 
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn="https://86a8cb4a29981dddfd025a9843c7279b@o4507296462012416.ingest.us.sentry.io/4507296481411072",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
 
 def main():
     ui = ExcelValidorUI()
@@ -14,9 +26,11 @@ def main():
 
         if error:
             ui.display_wrong_message
+            sentry_sdk.capture_message("A planilha Excel estava errada")
         elif ui.display_save_button():
             excel_to_sql(df)
             ui.display_sucess_message()
+            sentry_sdk.capture_message("O Banco SQL foi atualizado")
 
 
 if __name__ == "__main__":
